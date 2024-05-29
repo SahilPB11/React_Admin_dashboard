9310dddd2b278e8f64128fb5da17c1bf88f5c595
Sahil <garg07825@gmail.com>
2024-05-29 18:09:55 +0530

    added files and set up the avatar

--- src/components/layout/current_user.tsx
+++ src/components/layout/current_user.tsx
@@ -1,7 +1,48 @@
-import { Popover } from 'antd'
-import React from 'react'
+import { Button, Popover } from 'antd'
+import { useState } from 'react'
+import CustomAvatar from '../custom_avatar'
+import { useGetIdentity } from '@refinedev/core'
+import { Text } from "../text"
+import type { User } from "@/graphql/schema.types"
+import { SettingOutlined } from '@ant-design/icons'
 
 const CurrentUser = () => {
+    const { data: user } = useGetIdentity<User>()
+    const [isOpen, setIsOpen] = useState(false)
+    const content = (
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+            }}
+        >
+            <Text
+                strong
+                style={{ padding: "12px 20px" }}
+            >
+                {user?.name}
+            </Text>
+            <div
+            style={{
+                borderTop: "1px solid #d9d9d9",
+                padding:"4px",
+                display: "flex",
+                flexDirection: "column",
+                gap: "4px",
+            }}
+            >
+                <Button style={{ textAlign: "left" }}
+                    icon={<SettingOutlined />}
+                    type='text'
+                    block
+                    onClick={() => setIsOpen(true)}
+                >
+                    Acount Settings
+                </Button>
+            </div>
+        </div>
+    )
+
     return (
         <>
             <Popover
@@ -9,8 +50,14 @@ const CurrentUser = () => {
                 trigger={'click'}
                 overlayInnerStyle={{ padding: 0 }}
                 overlayStyle={{ zIndex: 999 }}
+                content={content}
             >
-                Test
+                <CustomAvatar
+                    name={user?.name}
+                    src={user?.avatarUrl}
+                    size="default"
+                    style={{ cursor: 'pointer' }}
+                />
             </Popover>
         </>
     )
